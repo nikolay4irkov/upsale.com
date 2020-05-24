d98400c00f96ea8b8b9b399bd2814750e9fdc709
nikolaychirkov <nikolay4irkov@gmail.com>
2020-05-24 14:23:57 +0300

    section recommendations done (mob and desk)

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,19 +4,20 @@ if ($('.swiper-container').length) {
   let slider = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     loop: false,
-    freeMode: true,
+    freeMode: false,
     spaceBetween: 30,
     speed: 500,
     updateOnWindowResize: true,
 
     navigation: {
-      nextEl: 'swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next--rec',
+      prevEl: '.swiper-button-prev--rec',
     },
 
     breakpoints: {
       1200: {
         slidesPerView: 2,
+        spaceBetween: 20
       },
     }
 
